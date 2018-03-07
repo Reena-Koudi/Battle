93876711c66ef1a5966710f4d6e9a680dae5fd56
Reenakoudi <kreena92@gmail.com>
2018-03-07 15:58:54 +0000

    Refactor code using sessions

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+enable :sessions
 
 get '/' do
   erb :index
 end
 
 post '/names' do
-  @player1 = params[:player1]
-  @player2 = params[:player2]
+session[:player1] = params[:player1]
+session[:player2] = params[:player2]
+redirect('/names')
+end
+
+get '/names' do
+  @player1 = session[:player1]
+  @player2 = session[:player2]
   erb :result
 end
   run! if app_file == $0
